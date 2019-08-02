b61cb626d7be654393d57be6a7390dba8ca7d591
Parveen Kumar <parveen@winuxsoftwaresolutions.com>
2019-08-02 18:08:50 +0530

    about us page updated with repeat function

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,75 +1,37 @@
 import React from 'react';
 import imgbanner from '../images/img-about.jpg';
-import iconabout1 from '../images/icon-about-1.png';
-import iconabout2 from '../images/icon-about-2.png';
-import iconabout3 from '../images/icon-about-3.png';
-const About = () => {
-	return (
-		<div className="row">
-			<div className="about">
-				<div className="banner-about">
-					<img src={imgbanner} alt="about banner"/>
+import Navigation from '../json/navigation.json';
+
+export const CardItems = ({item}) => {
+		return (
+			<div className="card col-md-6 col-lg-3 tc">
+				<div className="card-img pb-3">
+					<img src={require(`../images/${item.icon}`)} alt={item.alt}/>
 				</div>
+				<div className="card-box">
+					<h4 className="card-title py-3 mbr-fonts-style display-5">
+					{ item.text }</h4>
+					<p className="mbr-text mbr-fonts-style display-7">
+						{item.para}
+					</p>
+				</div>
+			</div>
+	 );	
+}
 
-				<div className="container">
-					<div className="row">
-						<div className="card col-md-6 col-lg-3 tc">
-							<div className="card-img pb-3">
-								<img src={iconabout1} alt="icon"/>
-							</div>
-							<div className="card-box">
-								<h4 class="card-title py-3 mbr-fonts-style display-5">
-									Creativity</h4>
-								<p class="mbr-text mbr-fonts-style display-7">
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, placeat. Repellat laudantium eligendi ab consequatur animi quidem minus aperiam, eum reiciendis.
-								</p>
-							</div>
-						</div>
-
-						<div className="card col-md-6 col-lg-3 tc">
-							<div className="card-img pb-3">
-								<img src={iconabout2} alt="icon"/>
-							</div>
-							<div className="card-box">
-								<h4 class="card-title py-3 mbr-fonts-style display-5">
-								Worldwide</h4>
-								<p class="mbr-text mbr-fonts-style display-7">
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, placeat. Repellat laudantium eligendi ab consequatur animi quidem minus aperiam, eum reiciendis.
-								</p>
-							</div>
-						</div>
-
-						<div className="card col-md-6 col-lg-3 tc">
-							<div className="card-img pb-3">
-								<img src={iconabout3} alt="icon"/>
-							</div>
-							<div className="card-box">
-								<h4 class="card-title py-3 mbr-fonts-style display-5">
-								Unique Styles</h4>
-								<p class="mbr-text mbr-fonts-style display-7">
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, placeat. Repellat laudantium eligendi ab consequatur animi quidem minus aperiam, eum reiciendis.
-								</p>
-							</div>
-						</div>
-
-
-						<div className="card col-md-6 col-lg-3 tc">
-							<div className="card-img pb-3">
-								<img src={iconabout3} alt="icon"/>
-							</div>
-							<div className="card-box">
-								<h4 class="card-title py-3 mbr-fonts-style display-5">
-								Unique Styles</h4>
-								<p class="mbr-text mbr-fonts-style display-7">
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, placeat. Repellat laudantium eligendi ab consequatur animi quidem minus aperiam, eum reiciendis.
-								</p>
-							</div>
-						</div>
-
-					</div>
+const About = () => (
+	<div className="row">
+		<div className="about">
+			<div className="banner-about">
+				<img src={imgbanner} alt="{key.alt}"/>
+			</div>
+			<div className="container">
+				<div className="row">
+					{ Navigation.sidenav.map((item, index) => (<CardItems key={ index } item={item} />)) }
 				</div>
 			</div>
 		</div>
-	)
-}
+	</div>
+);
+
 export default About ;
