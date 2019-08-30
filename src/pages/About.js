8a09312381a170ac0544c5f133daa6617d1821ca
MeenShar <meenakshi.sharma@pearson.com>
2019-08-30 14:46:59 +0530

    service page updated with dynmic data

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ import Navigation from '../json/navigation.json';
 export const CardItems = ({item}) => {
 		return (
 			<div className="card col-md-6 col-lg-3 tc">
-				<div className="card-img pb-3">
+				<div className="card-img pb-3 pt-3">
 					<img src={require(`../images/${item.icon}`)} alt={item.alt}/>
 				</div>
 				<div className="card-box">
@@ -25,7 +25,7 @@ const About = () => (
 			<div className="banner-about">
 				<img src={imgbanner} alt="{key.alt}"/>
 			</div>
-			<div className="container">
+			<div className="mt-4 container">
 				<div className="row">
 					{ Navigation.sidenav.map((item, index) => (<CardItems key={ index } item={item} />)) }
 				</div>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,9 +1,81 @@
 import React from 'react';
-const Service = () => {
-	return (
-		<div className="services">
-			This is service page.
+import SBanner from '../images/img-service.jpg';
+import SData from '../json/service_data.json';
+const Service = () => (
+	<div className="row">
+		<div className="about">
+		{SData.service_page.map((SDetails, index) => {
+					return <div>
+			<div className="banner-about">
+				<h2>{SDetails.banner_title}</h2>
+				<p>{SDetails.banner_text}</p>
+				<img src={SBanner} alt="{key.alt}"/>
+			</div>
+			<div className="service-data">
+				<div className="container">
+					<div className="row">
+						<div className="whatwedo">
+							<h3>{SDetails.whatwedo}</h3>
+							<p>{SDetails.whatwedo_text}</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="container">
+					<div className="row">
+						<div className="col-md-4 mb-4">
+							<i class="fas fa-atom"></i>
+							<h5 className="tc">{SDetails.para}</h5>
+							<p className="tc">{SDetails.text}</p>
+						</div>
+						<div className="col-md-4 mb-4">
+							<i class="fas fa-laptop-code"></i>
+							<h5 className="tc">{SDetails.para}</h5>
+							<p className="tc">{SDetails.ptext}</p>
+						</div>
+						<div className="col-md-4 mb-4">
+							<i class="fas fa-sitemap"></i>
+							<h5 className="tc">{SDetails.para}</h5>
+							<p className="tc">{SDetails.ptext}</p>
+						</div>
+						<div className="col-md-4 mb-4">
+							<i class="fas fa-sitemap"></i>
+							<h5 className="tc">{SDetails.para}</h5>
+							<p className="tc">{SDetails.ptext}</p>
+						</div>
+						<div className="col-md-4 mb-4">
+							<i class="fas fa-laptop-code"></i>
+							<h5 className="tc">{SDetails.para}</h5>
+							<p className="tc">{SDetails.ptext}</p>
+						</div>
+						<div className="col-md-4 mb-4">
+							<i class="fas fa-atom"></i>
+							<h5 className="tc">{SDetails.para}</h5>
+							<p className="tc">{SDetails.text}</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="service-bottom-img">
+					<div className="container-fluid">
+						<div className="row">
+							<div className="dflex">
+								<h5>{SDetails.bottom_heading}</h5>
+								<p>{SDetails.bottom_text}</p>
+								<div className="btn-outline-white">
+									Call Us
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	)
-}
-export default Service;
+
+})
+}	
+</div>
+</div>
+);
+
+export default Service ;
